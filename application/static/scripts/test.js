0608dc39ee0c9702253eb3cb72389b179d9204ad
carbr059 <carbr059@student.liu.se>
2016-02-19 13:18:49 +0100

    Start skeleton, add meta tags

--- application/static/scripts/test.js
+++ application/static/scripts/test.js
@@ -1,7 +1,29 @@
-var CellList = React.createClass({
+var Container = React.createClass({
     render: function() {
         return (
             <div className="cells">
+                <TopDiv />
+                <NavigationHub />
+                <Diagram />
+            </div>
+        );
+    }
+});
+var TopDiv = React.createClass({
+    render: function() {
+        return (
+            <div className="top">
+                <h1>Sample Cell 1</h1>
+                <h2>Sample Cell 2</h2>
+                <h3>Sample Cell 3</h3>
+            </div>
+        );
+    }
+});
+var NavigationHub = React.createClass({
+    render: function() {
+        return (
+            <div className="navigation">
                 <h1>Sample Cell 1</h1>
                 <h2>Sample Cell 2</h2>
                 <h3>Sample Cell 3</h3>
@@ -10,7 +32,20 @@ var CellList = React.createClass({
     }
 });
 
+var Diagram = React.createClass({
+    render: function() {
+        return (
+            <div className="diagram">
+                <h1>Sample Cell 1</h1>
+                <h2>Sample Cell 2</h2>
+                <h3>Sample Cell 3</h3>
+            </div>
+        );
+    }
+});
+
+
 ReactDOM.render(
-    <CellList />,
+    <Container />,
     document.getElementById('content')
 );
