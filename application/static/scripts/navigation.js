b92843705a15bcb0c0663389a20929ea15ab3344
Ulmstedt <matte___93@hotmail.com>
2016-03-02 17:32:02 +0100

    Add full API support

--- application/static/scripts/navigation.js
+++ application/static/scripts/navigation.js
@@ -1,147 +1,70 @@
-var data1 =
-{
-    name: "Cancer",
-    children:
-    [
-        {name: "Lung"},
-        {name: "Breast"},
-        {name: "Prostate"},
-        {name: "Foot"},
-        {name: "Brain"}
-    ]
-};
-
-var data2 = 
-{
-    name: "Breast",
-    children:
-    [
-        {name: "Nipple"},
-        {name: "Soft"},
-        {name: "Big"},
-        {name: "Warts"},
-    ]
-};
-
-
-var data3 = {
-    name: "Lung",
-    children:
-    [
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"}
-    ]
-};
-
-var data4 = {
-    name: "Prostate",
-    children:
-    [
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"}
-    ]
-};
-
-var data5 = {
-    name: "Foot",
-    children:
-    [
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"}
-    ]
-};
-
-var data6 = {
-    name: "Brain",
-    children:
-    [
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"}
-    ]
-};
-
-var data7 = {
-    name: "Nipple",
-    children:
-    [
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"},
-        {name: "PlaceHolder"}
-    ]
-};
-
-var dataArray = [data1, data2, data3, data4, data5, data6, data7]; //Dummy data instead of API
 
 //Navigation panel component
 var Navigation = React.createClass({
-    //Set current data set
-    setDataSet: function(name){
-		//TODO: Ska göra anrop till api istället
-        for(var i=0; i < this.props.data.length; i++){
-            if(this.props.data[i].name === name){
-                this.setState({dataSet: this.props.data[i]});
-                break;
-            }
-        }
-		
-    },
-	
-	
-	componentDidMount: function(){
-
-		var queryResult=[];
-
-        //TODO: change url to the correct one when we know it works
-        $.ajax({
+    //Set current data set, get children of id
+    setParent: function(id){
+        var queryResult=[];
+        //Get children
+		$.ajax({
             type: "GET",
             "method": "GET",
-            url: 'http://private-anon-f027ed251-sctsnapshotrestapi.apiary-mock.com/api/snomed/en-edition/v20140731/concepts/123037004/children?form=inferred%27%29',
-            //url: 'http://browser.ihtsdotools.org/api/snomed/en-edition/v20140731/descriptions?query=asthma&searchMode=partialMatching&lang=english&statusFilter=english&skipTo=0&returnLimit=5&normalize=true',
+            url: 'http://79.136.62.204:3000/snomed/en-edition/v20150731/concepts/' + id + '/children?form=inferred=true',
             dataType: "json",
             error: function(){
                 console.log('Failed to access API')
             },
             success: function(result){
-                console.log("api success")
                 for (var i in result){
                     queryResult.push({
-                        name: result[i]["defaultTerm"],
-                        id: result[i]["conceptId"]
+                        name: result[i].defaultTerm,
+                        id: result[i].conceptId
                     })
                 }
+                //Ignore if no children were found
+                if(queryResult.length !== 0){
+                    this.setState({
+                        termChildren: queryResult,
+                        currentID: id
+                    });
+                }
+            }.bind(this)
+        });
+        //Get parent name
+        $.ajax({
+            type: "GET",
+            "method": "GET",
+            url: 'http://79.136.62.204:3000/snomed/en-edition/v20150731/concepts/' + id,
+            dataType: "json",
+            error: function(){
+                console.log('Failed to access API')
+            },
+            success: function(result){
+                console.log(result.defaultTerm);
                 this.setState({
-                    //dataSet: queryResult //Lägg till igen när api används
+                    currentParent: result.defaultTerm
                 });
             }.bind(this)
-
         });
+    },
 	
+	
+	componentDidMount: function(){
+        this.setParent(138875005);
 	},
 	
     //Handles clicks on the children (callback function)
     handleClick: function(e){
-        this.state.history.push(this.state.dataSet.name);
-        this.setDataSet(e.name);
+        this.state.history.push(this.state.currentID);
+        console.log(this.state.history);
+        this.setParent(e.id);
+        
     },
 
     //Move up one level in the tree (from history)
     upOneLevel: function(){
-        var name = this.state.history.pop();
-        this.setDataSet(name);
+        var id = this.state.history.pop();
+        console.log(id);
+        this.setParent(id);
     },
 
     //Initial state of the component
@@ -149,7 +72,7 @@ var Navigation = React.createClass({
         return (
             {
 				currentParent: 'SNOMED CT Concept',
-                dataSet: this.props.data[0], //[]
+                termChildren: [], //this.props.data[0]
                 history: []
             }
         );
@@ -163,14 +86,17 @@ var Navigation = React.createClass({
             backArrow = {};
         }
         //Create NavigationItem's for all the children of the current parent node
-        var ItemArray = this.state.dataSet.children.map(function(child){ //ta bort .children för api
+        var ItemArray = this.state.termChildren.map(function(child){ //ta bort .children för api
             return(
-                <NavigationItem name={child.name} handleClickCallback={this.handleClick} />
+                <NavigationItem key={child.id} id={child.id} name={child.name} handleClickCallback={this.handleClick} />
             );
         }, this);
         return (
             <div className="list-group navigation">
-                    <a className="list-group-item active" onClick={this.upOneLevel} href="#"><span style={backArrow} className="glyphicon glyphicon-triangle-top" aria-hidden="true"></span> {this.state.dataSet.name}</a> 
+                    <a className="list-group-item active" onClick={this.upOneLevel} href="#">
+                        <span style={backArrow} className="glyphicon glyphicon-triangle-top" aria-hidden="true"> </span> 
+                        {this.state.currentParent}
+                    </a> 
                     {ItemArray}
             </div>
         );
@@ -180,20 +106,23 @@ var Navigation = React.createClass({
 //Clickable items in the navigation bar
 var NavigationItem = React.createClass({
     //Handles clicks on the items and calls back to the parent to change the current data
-    handleClick: function(name){
-        this.props.handleClickCallback({name: name});
+    handleClick: function(e){
+        this.props.handleClickCallback(e);
     },
 
     render: function(){
+        var arrowStyle = {fontSize: "1.2em", position:"absolute", right:"10px", top:"25%"};
         return(
             <div>
-                <a className="list-group-item" onClick={this.handleClick.bind(this, this.props.name)} href='#'>{this.props.name}<span style={{fontSize: "1.2em", position:"absolute", right:"10px", top:"25%"}} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span></a>
+                <a className="list-group-item" onClick={this.handleClick.bind(this, {name:this.props.name, id:this.props.id})} href='#'>{this.props.name}
+                    <span style={arrowStyle} className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                </a>
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <Navigation data={dataArray} />,
+    <Navigation />,
     document.getElementById('navContainer')
 );
