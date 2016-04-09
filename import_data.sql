0547d1db09f03d4833e02f67ccf1e4a8ae6c6ea6
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-10 00:06:53 +0200

    Remove outcommenting comments

--- import_data.sql
+++ import_data.sql
@@ -1,22 +1,22 @@
--- COPY concept(id, effective_time, active, module_id, definition_status_id)
--- FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Concept_Full_INT_20150731.txt'
--- WITH (FORMAT csv, HEADER true, DELIMITER '	');
--- 
--- COPY description(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
--- FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Description_Full-en_INT_20150731.txt'
--- WITH (FORMAT csv, HEADER true, DELIMITER '	');
--- 
--- COPY text_definition(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
--- FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_TextDefinition_Full-en_INT_20150731.txt'
--- WITH (FORMAT csv, HEADER true, DELIMITER '	');
--- 
--- COPY relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id, characteristic_type_id, modifier_id)
--- FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Relationship_Full_INT_20150731.txt'
--- WITH (FORMAT csv, HEADER true, DELIMITER '	');
--- 
--- COPY stated_relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id,  characteristic_type_id, modifier_id)
--- FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_StatedRelationship_Full_INT_20150731.txt'
--- WITH (FORMAT csv, HEADER true, DELIMITER '	');
+COPY concept(id, effective_time, active, module_id, definition_status_id)
+FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Concept_Full_INT_20150731.txt'
+WITH (FORMAT csv, HEADER true, DELIMITER '	');
+
+COPY description(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
+FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Description_Full-en_INT_20150731.txt'
+WITH (FORMAT csv, HEADER true, DELIMITER '	');
+
+COPY text_definition(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
+FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_TextDefinition_Full-en_INT_20150731.txt'
+WITH (FORMAT csv, HEADER true, DELIMITER '	');
+
+COPY relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id, characteristic_type_id, modifier_id)
+FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_Relationship_Full_INT_20150731.txt'
+WITH (FORMAT csv, HEADER true, DELIMITER '	');
+
+COPY stated_relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id,  characteristic_type_id, modifier_id)
+FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Terminology/sct2_StatedRelationship_Full_INT_20150731.txt'
+WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY language_refset(id, effective_time, active, module_id, refset_id, referenced_component_id, acceptability_id)
 FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Full/Refset/Language/der2_cRefset_LanguageFull-en_INT_20150731.txt'
