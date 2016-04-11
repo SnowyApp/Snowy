0bce9653a0cf3cf01264a268ffbc104771c9352c
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-11 14:43:00 +0200

    Add get_children to sample

--- sample.py
+++ sample.py
@@ -11,3 +11,5 @@ auth = {"Authorization": json.loads(data.text)["token"]}
 def search(term):
     return json.loads(requests.get(url + "/search/cancer").text)['hits']['hits']
 
+def get_children(id):
+    return json.loads(requests.get(url + "/get_children/" + str(id)).text)
