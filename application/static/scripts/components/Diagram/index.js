a1a7b3ba139da30fffa4840c751ae042c407678b
Emil Nilsson <emini215@student.liu.se>
2016-04-14 23:24:05 +0200

    Update Diagram/index.js state on change of props and requests

--- application/static/scripts/components/Diagram/index.js
+++ application/static/scripts/components/Diagram/index.js
@@ -7,10 +7,83 @@ module.exports = React.createClass({
     */
     getInitialState: function() {
         return {
-            data: this.props.data || treeData1,
-            domain: this.props.data || { x: [0, 100], y: [0, 100] }
+            data: this.update(this.props.sctid),
+            domain: this.props.domain || { x: [0, 100], y: [0, 100] }
         };
     },
+
+    getRoot: function(sctid) {
+        $.ajax({
+            type: "GET",
+            "method": "GET",
+            url: this.props.url + "/concept/" + sctid,
+            dataType: "json",
+            error: function() {
+                console.log("Failed to retrieve Diagram root.");
+            },
+            success: function(result) {
+                var children = [];
+                if (this.state.data !== undefined) {
+                    children = this.state.data.slice()[0].children;
+                }
+
+                this.setState({
+                    data: [
+                        {
+                            "name": result.term,
+                            "concept_id": result.id,
+                            "parent": "null",
+                            "children": children
+                        }
+                    ]
+                });
+            }.bind(this)
+        });
+    },
+
+    getChildren: function(sctid) {
+        $.ajax({
+            type: "GET",
+            "method": "GET",
+            url: this.props.url + "/get_children/" + sctid,
+            dataType: "json",
+            error: function() {
+                console.log("Failed to retrieve Diagram children.");
+            },
+            success: function(result) {
+                var children = [];
+                for (var i in result) {
+                    children.push({
+                        name: result[i].term,
+                        concept_id: result[i].id,
+                        parent: sctid
+                    });
+                }
+                
+                var dataCopy = this.state.data.slice();
+                dataCopy[0].children = children;
+                this.setState({
+                    data: dataCopy
+                });
+
+            }.bind(this)
+        });
+    },
+
+    update: function(sctid) {
+        console.log("UPDATE:" + sctid);
+        if (sctid != null) {
+            this.getRoot(sctid);
+            this.getChildren(sctid);
+        }
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        console.log("PROPS UPDATED");
+        this.update(nextProps.sctid);
+        console.log("PROPS DONE UPDATING");
+    },
+
     /**
     * Render the diagram from the current state.
     */
