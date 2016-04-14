3887adb55a304683efea373c273ba8b188b0b8bb
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-14 09:58:29 +0200

    Add sample function for udate_info

--- sample.py
+++ sample.py
@@ -13,3 +13,7 @@ def search(term):
 
 def get_children(id):
     return json.loads(requests.get(url + "/get_children/" + str(id)).text)
+
+def update_info(first_name, last_name, language):
+    data = {"first_name": first_name, "last_name": last_name, "language": language}
+    return json.loads(requests.post(url + "/update_info", json=data, headers=auth).text)
