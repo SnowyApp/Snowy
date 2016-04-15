34c61fa971cbed75db7f2db698615bc4070d555c
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-15 11:15:14 +0200

    Add sample for diagrams

--- sample.py
+++ sample.py
@@ -17,3 +17,10 @@ def get_children(id):
 def update_info(first_name, last_name, language):
     data = {"first_name": first_name, "last_name": last_name, "language": language}
     return json.loads(requests.post(url + "/update_info", json=data, headers=auth).text)
+
+def store_diagram():
+    data = {"data": "This is a test diagram"}
+    return json.loads(requests.post(url + "/diagram", json=data, headers=auth).text)
+
+def get_diagram():
+    return json.loads(requests.get(url + "/diagram", headers=auth).text)
