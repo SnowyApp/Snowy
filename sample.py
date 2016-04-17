63c0680b6284ec3bfd2b3e396959598084f5e16d
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-17 18:13:33 +0200

    Add sample for retrieving concept

--- sample.py
+++ sample.py
@@ -29,6 +29,9 @@ def search(term):
 def get_children(id):
     return json.loads(requests.get(url + "/get_children/" + str(id)).text)
 
+def get_concept(id):
+    return json.loads(requests.get(url + "/concept/" + str(id)).text)
+
 def update_info(first_name, last_name, language):
     data = {"first_name": first_name, "last_name": last_name, "language": language}
     return json.loads(requests.post(url + "/update_info", json=data, headers=auth).text)
