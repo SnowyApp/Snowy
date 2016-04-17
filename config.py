ba04fafced36a87d9a96ff613325b885de762303
Carl Brage <carbr059@student.liu.se>
2016-04-17 17:27:37 +0200

    Revert changes in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 STATIC_FOLDER = 'static'
 STATIC_URL_PATH = ''
 
-SQLALCHEMY_DATABASE_URI ="sqlite:///test.db"
+SQLALCHEMY_DATABASE_URI ="sqlite:////tmp/tmp.db"
 SECRET_KEY = 'tkar8j;h{)*JYLUJ#IEonareinskJ@{*(L{ONOEINIDC'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
