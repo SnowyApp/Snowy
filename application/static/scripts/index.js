81008a85f73130eb9cc86415398123adedbdcb50
Joacim S <eletexel@hotmail.com>
2016-04-19 10:56:19 +0200

    fix locally working

--- application/static/scripts/index.js
+++ application/static/scripts/index.js
@@ -17,7 +17,7 @@ var brageUrl = 'http://155.4.145.248:3000/snomed/en-edition/v20150731/descriptio
 //Use this if none of the above work, can only search on asthma
 var mockApi ='http://private-anon-d3abcd99e-snomedctsnapshotapi.apiary-mock.com/api/snomed/en-edition/v20160131/descriptions?query=&searchMode=partialMatching&lang=english&statusFilter=english&skipTo=0&returnLimit=5&normalize=true';
 
-
+//'http://85.229.227.71:5000''http://localhost:5000'
 var Container = React.createClass({
     getInitialState: function(){
         return{
--- sql-scripts/import_data.sql
+++ sql-scripts/import_data.sql
@@ -1,39 +1,39 @@
 COPY concept(id, effective_time, active, module_id, definition_status_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Concept_Snapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Concept_Snapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY description(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Description_Snapshot-en_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Description_Snapshot-en_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY text_definition(id, effective_time, active, module_id, concept_id, language_code, type_id, term, case_significance_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_TextDefinition_Snapshot-en_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_TextDefinition_Snapshot-en_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id, characteristic_type_id, modifier_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Relationship_Snapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_Relationship_Snapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY stated_relationship(id, effective_time, active, module_id, source_id, destination_id, relationship_group, type_id,  characteristic_type_id, modifier_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_StatedRelationship_Snapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Terminology/sct2_StatedRelationship_Snapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY language_refset(id, effective_time, active, module_id, refset_id, referenced_component_id, acceptability_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot-en_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot-en_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY association_refset(id, effective_time, active, module_id, refset_id, referenced_component_id, target_component_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_cRefset_AssociationReferenceSnapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_cRefset_AssociationReferenceSnapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY attribute_value_refset(id, effective_time, active, module_id, refset_id, referenced_component_id, value_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_cRefset_AttributeValueSnapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_cRefset_AttributeValueSnapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY simple_refset(id, effective_time, active, module_id, refset_id, referenced_component_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_Refset_SimpleSnapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Content/der2_Refset_SimpleSnapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
 
 COPY complex_map_refset(id, effective_time, active, module_id, refset_id,  referenced_component_id, map_group, map_priority, map_rule,  map_advice, map_target, correlation_id)
-FROM '/Users/simon/Projects/browser/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Map/der2_iissscRefset_ComplexMapSnapshot_INT_20150731.txt'
+FROM 'C:\DB-browser\SnomedCT_Release_SE_7.0.1_20151130/SnomedCT_RF2Release_INT_20150731/Snapshot/Refset/Map/der2_iissscRefset_ComplexMapSnapshot_INT_20150731.txt'
 WITH (FORMAT csv, HEADER true, DELIMITER '	');
