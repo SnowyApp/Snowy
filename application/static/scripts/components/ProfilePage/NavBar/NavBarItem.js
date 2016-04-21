f9f98999324cfe6bc5fae36d9299b9b328150347
Mattias Ulmstedt <matte___93@hotmail.com>
2016-04-21 16:29:30 +0200

    Transform onSelect argument to lower case

--- application/static/scripts/components/ProfilePage/NavBar/NavBarItem.js
+++ application/static/scripts/components/ProfilePage/NavBar/NavBarItem.js
@@ -6,7 +6,7 @@ module.exports = React.createClass({
         var navBarItemClass = (this.props.active ? 'active' : null) + " tabItem"; //Highlight active tab
         return(
             <li role="presentation" className={navBarItemClass}>
-                <a href="#" onClick={this.props.onSelect.bind(null, this.props.name)}>
+                <a href="#" onClick={this.props.onSelect.bind(null, this.props.name.toLowerCase())}>
                     {this.props.name}
                 </a>
             </li>
