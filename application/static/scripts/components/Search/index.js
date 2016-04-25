2c80b5f5fc082fb14b0df5660e8a045cc586924a
Mattias Ulmstedt <matte___93@hotmail.com>
2016-04-25 16:21:37 +0200

    Add comment

--- application/static/scripts/components/Search/index.js
+++ application/static/scripts/components/Search/index.js
@@ -11,6 +11,7 @@ module.exports = React.createClass({
  Handles user input
  */
 var SearchBox = React.createClass({
+    //Dictionary for supported languages
     dict: {
         se: {
             "search":       "Sök"
