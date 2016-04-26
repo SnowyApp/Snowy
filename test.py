d9a9ff2dd43133d8d67e32be49b2c2dd1f91fe32
Simon Lindblad <simon.lindblad93@gmail.com>
2016-04-26 16:51:02 +0200

    Update tests to support new database

--- test.py
+++ test.py
@@ -3,10 +3,21 @@ import json
 import unittest
 import psycopg2
 
+DB_NAME = "snomedct"
+DB_USER = "simon"
+
 class TestApplication(unittest.TestCase):
     """
     Performs unittests on the application.
     """
+    def drop_database(self):
+        conn = psycopg2.connect("dbname=" + DB_NAME + " user=" + DB_USER)
+        conn.autocommit = True
+        cur = conn.cursor()
+        with conn as c:
+            f = open("sql-scripts/usr_schema.sql", "r")
+            c.cursor().execute(f.read())
+            f.close()
 
     def setUp(self):
         """
@@ -15,6 +26,10 @@ class TestApplication(unittest.TestCase):
         """
         application.app.config['TESTING'] = True
         self.app = application.app.test_client()
+        self.drop_database()
+
+    def tearDown(self):
+        self.drop_database()
 
     def create_user(self):
         user_data = {"email": "simli746@student.liu.se", "password": "emini"}
@@ -28,7 +43,7 @@ class TestApplication(unittest.TestCase):
 
     def test_create_user(self):
         response = self.create_user()
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
         # The user is already registered
         response = self.create_user()
@@ -67,7 +82,5 @@ class TestApplication(unittest.TestCase):
         response = self.app.get('/verify', headers={'Authorization': data['token']})
         self.assertEqual(response.status_code, 401)
         
-        
-
 if __name__ == "__main__":
     unittest.main()
