d3fc8ac52dfccccf345d30d7cf21e61fb5d64b2d
Mattias Ulmstedt <matte___93@hotmail.com>
2016-04-27 17:30:07 +0200

    Use root name as panel header

--- application/static/scripts/components/Diagram/InfoPanel.js
+++ application/static/scripts/components/Diagram/InfoPanel.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 require('bootstrap');
 
 var InfoPanel = React.createClass({
+    propTypes: {
+        hidePanel:      React.PropTypes.func,
+        data:           React.PropTypes.array
+    },
+
     render: function(){
+        const termName = (this.props.data.length > 0 ? this.props.data[0].name : "");
         return (
             <div className="panel panel-default infoPanel">
-                <div className="panel-heading infoPanelHandle">Panel heading without title</div>
+                <div className="panel-heading infoPanelHandle">{termName}</div>
                 <div className="panel-body">
                     Panel content
                 </div>
