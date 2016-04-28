5e9b5b0bbedacfb06981426280b7bd3c2e3f8af2
Mattias Ulmstedt <matte___93@hotmail.com>
2016-04-28 12:44:40 +0200

    Add parents table to info panel

--- application/static/scripts/components/Diagram/InfoPanel.js
+++ application/static/scripts/components/Diagram/InfoPanel.js
@@ -1,25 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import cookie from 'react-cookie';
 require('bootstrap');
 
 var InfoPanel = React.createClass({
     propTypes: {
         hidePanel:      React.PropTypes.func,
         data:           React.PropTypes.array,
-        language:       React.PropTypes.string
+        language:       React.PropTypes.string,
+        url:            React.PropTypes.string
     },
-
+    //Dict with supported languages
     dict: {
         se: {
-            termInfo:           "Terminformation"
+            termInfo:           "Terminformation",
+            name:               "Namn",
+            parents:            "Föräldrar"
         },
         en: {
-            termInfo:           "Term information"
+            termInfo:           "Term information",
+            name:               "Name",
+            parents:            "Parents"
+        }
+    },
+
+    getInitialState: function(){
+        return ({
+            parents: []
+        });
+    },
+
+    componentWillReceiveProps: function(nextProps){
+        //this.getParents(nextProps.data[0].concept_id);
+        console.log(nextProps);
+    },
+
+    getParents: function(id){
+        console.log("HEJ");
+        if (cookie.load('userId') != null) {
+            $.ajax({
+                method: "GET",
+                url: this.props.url + "/get_parents/" + id,
+                success: function (data) {
+                    console.log(data);
+                    var parents = [];
+                    for(var i = 0; i < data.length; i++){
+                        parents.push({
+                            id: data[i].id,
+                            name: data[i].term
+                        });
+                    }
+                    this.setState({
+                        parents: parents
+                    });
+                }.bind(this),
+                error: function (textStatus, errorThrown) {
+                    console.log(textStatus);
+                    console.log(errorThrown);
+                    console.log("Failed getting parents.");
+                },
+                contentType: "application/json",
+                dataType: "json"
+            });
         }
     },
 
     render: function(){
-        const termName = (this.props.data.length > 0 ? this.props.data[0].name : "");
         return (
             <div className="panel panel-default infoPanel">
                 <div className="panel-heading infoPanelHandle">
@@ -29,7 +75,24 @@ var InfoPanel = React.createClass({
                     </button>
                 </div>
                 <div className="panel-body">
-                    Panel content
+                    <table className="table table-condensed">
+                        <caption>{this.dict[this.props.language]["parents"]}</caption>
+                        <thead>
+                            <tr>
+                                <th id="parent_name" className="parentsTable">
+                                    {this.dict[this.props.language]["name"]} 
+                                </th>
+                                <th id="parent_id" className="parentsTable">
+                                    ID 
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><td>Environment or geographical location hej hej hej hej hej (environment / location)</td><td>900000000000441003</td></tr>
+                            <tr><td>Environment or geographical location hej hej hej hej hej (environment / location)</td><td>900000000000441003</td></tr>
+                            <tr><td>Environment or geographical location hej hej hej hej hej (environment / location)</td><td>900000000000441003</td></tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         );
