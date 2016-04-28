9bc7cb7c4cdb8ab423417d52b1b53175f5335f11
Mattias Ulmstedt <matte___93@hotmail.com>
2016-04-28 11:56:30 +0200

    Add close button

--- application/static/scripts/components/Diagram/InfoPanel.js
+++ application/static/scripts/components/Diagram/InfoPanel.js
@@ -12,7 +12,12 @@ var InfoPanel = React.createClass({
         const termName = (this.props.data.length > 0 ? this.props.data[0].name : "");
         return (
             <div className="panel panel-default infoPanel">
-                <div className="panel-heading infoPanelHandle">{termName}</div>
+                <div className="panel-heading infoPanelHandle">
+                    {termName}
+                    <button onClick={this.props.hidePanel} className="close closeInfoButton" type="button" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
                 <div className="panel-body">
                     Panel content
                 </div>
