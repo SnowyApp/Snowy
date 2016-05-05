f083eb1fbf69d5ec5386dd81ccfc95589598cf6a
Carl Brage <carbr059@student.liu.se>
2016-05-05 14:49:57 +0200

    Switch back to text-anchor middle

--- application/static/scripts/components/ConceptDefinitionDiagram/index.js
+++ application/static/scripts/components/ConceptDefinitionDiagram/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Maximum width of text before starting new line
-const TEXT_MAX_WIDTH = 230;
-// Width of the nodes
-const NODE_WIDTH = 250;
 // The margin between every level of attributes and concepts
 const LEVEL_MARGIN = 10;
 // Height of the nodes
 const NODE_HEIGHT = 50;
 // Margin between attributes and concepts (length of arrows)
+const NODE_MARGIN = 20;
+// Margin between the text and the outline of the node
 const WIDTH_MARGIN = 20;
 // Styling according to SNOMED CT diagramming guidelines
 const CONCEPT_COLOR = '#99CCFF';
 const DEFINED_CONCEPT_COLOR = '#CCCCFF';
 const DEFINED_CONCEPT_BORDER = 4;
+const ATTRIBUTE_COLOR = '#FFFFCC';
 // The current level we are drawing on
 var level = 0;
 
@@ -118,6 +117,19 @@ var ConceptDefinitionDiagram = React.createClass({
         this.drawConcept(svg, data);
     },
 
+    drawAttributeGroup: function(element, concept){
+
+    },
+
+    drawConjunction: function(element, concept){
+
+    },
+
+    drawRelationalOperator: function(element, concept){
+
+    },
+
+
     /**
      * Draw the given concept.
      *
@@ -135,7 +147,14 @@ var ConceptDefinitionDiagram = React.createClass({
 
         // draw a rectangle and text
         this.drawConceptRectangle(g, concept);
+        this.drawConceptId(g, concept);
         this.drawConceptText(g, concept);
+        // Change the width of the rectangle, to fit entire text + margin
+        var textWidth = g.select("text.name").node().getBBox().width;
+        var totalWidth = textWidth + WIDTH_MARGIN;
+        g.select("rect").attr("width", totalWidth);
+        g.select("text.name").attr("x", totalWidth/2);
+        g.select("text.id").attr("x", totalWidth/2);
 
         // return the grouping element
         return g;
@@ -146,7 +165,6 @@ var ConceptDefinitionDiagram = React.createClass({
      */
     drawConceptRectangle: function(group, concept) {
         return group.append("rect")
-            .attr("x", 50)
             .attr("width", 50)
             .attr("height", 50)
             // apply the correct colours depending on definition status
@@ -154,17 +172,38 @@ var ConceptDefinitionDiagram = React.createClass({
                 ConceptDefinitionDiagram.PRIMITIVE_COLOR :
                 ConceptDefinitionDiagram.DEFINED_COLOR);
     },
+    /**
+     * Draw id inside given grouping element.
+     */
+    drawConceptId: function(group, concept) {
+        return group.append("text")
+            // position text centered in the concept
+            .attr("class", "id")
+            .attr("y", NODE_HEIGHT/4)
+            .attr("x", 50)
+            .attr("text-anchor", "middle")
+            .attr("dy", ".35em")
+            .attr("font-family", "Helvetica, Arial, Sans-Serif")
+            .attr("font-size", 10)
 
+            // use the full name if possible, if not available use the synonym
+            // and if neither is defined use a default "NO NAME" name.
+            .text(function() {
+                return concept.id;
+            })
+
+            .style("fill-opacity", 1);
+    },
     /**
      * Draw text inside given grouping element.
      */
     drawConceptText: function(group, concept) {
         return group.append("text")
             // position text centered in the concept
-            .attr("y", 25)
+            .attr("class", "name")
+            .attr("y", 3*NODE_HEIGHT/4)
             .attr("x", 50)
             .attr("text-anchor", "middle")
-
             .attr("dy", ".35em")
             .attr("font-family", "Helvetica, Arial, Sans-Serif")
 
