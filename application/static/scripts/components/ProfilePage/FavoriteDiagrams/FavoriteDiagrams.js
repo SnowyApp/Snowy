85972743fac0660f1c1556ac3e6ad0afda091140
Mattias Ulmstedt <matte___93@hotmail.com>
2016-05-05 10:26:01 +0200

    Remove unused saveDiagram function from FavoriteDiagrams

--- application/static/scripts/components/ProfilePage/FavoriteDiagrams/FavoriteDiagrams.js
+++ application/static/scripts/components/ProfilePage/FavoriteDiagrams/FavoriteDiagrams.js
@@ -20,28 +20,6 @@ var FavoriteDiagrams = React.createClass({
         dateSort:           React.PropTypes.func
     },
 
-    //TODO: REMOVE THIS FUNCTION
-    saveDiagram: function() {
-        $.ajax({
-            type: "POST",
-            method: "POST",
-            headers: {
-                "Authorization" : cookie.load("userId")
-            },
-            url: this.props.url + "/diagram",
-            contentType: "application/json",
-            data: JSON.stringify({
-                "data": JSON.stringify({test: "333"}),
-                "created": new Date().toString(),
-                "name": "Diagram 2"
-            }),
-            error: function(xhr) {
-                console.log(xhr);
-                console.log("Could not store diagram.");
-            }.bind(this)
-        });
-    },
-
     getInitialState: function(){
         return({
             diagrams: [],
