26a0c2f21088ba5949c7ebf9456530e6124b2229
Simon Lindblad <simon.lindblad93@gmail.com>
2016-05-08 19:59:19 +0200

    Update setup.sh to fix issues with paths

--- database/setup.sh
+++ database/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-#psql -f scripts/setup_structure.sql
-
 psql -f scripts/user_schema.sql users
 psql -f scripts/import_INT_20150731.sql snomedct
-psql -f scripts/INT_20150731_schema.sql tmp
+psql -f scripts/INT_20150731_schema.sql new_design
+
+python3 scripts/transfer_data.py snomedct new_design
