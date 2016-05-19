6f9c0baf6e261dcab499c90aa796508067f32368
Carl Brage <carbr059@student.liu.se>
2016-05-19 13:30:03 +0200

    Move import to top of file in init.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.elasticsearch import FlaskElasticsearch
 from flask.ext.cors import CORS
+from application import views
 
 app = Flask(__name__)
 CORS(app)
@@ -8,5 +9,4 @@ app.config.from_object("config")
 CORS(app)
 es = FlaskElasticsearch(app)
 
-from application import views
 
