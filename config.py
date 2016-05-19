9e1ffc5b32b00cb5488586e7903b3972a33f26d7
Carl Brage <carbr059@student.liu.se>
2016-05-19 14:06:45 +0200

    Add comments to config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+"""
+Contains configuration constants for the backend.
+"""
+# The name of the folder containing the static files
 STATIC_FOLDER = 'static'
+# The path to the static folder
 STATIC_URL_PATH = ''
-
+# Used for generating a token
 SECRET_KEY = 'tkar8j;h{)*JYLUJ#IEonareinskJ@{*(L{ONOEINIDC'
-
+# The name of the database stored locally
 DB_NAME = "snomedct"
+# The name of the user holding the database
 DB_USER = "simon"
